e4420fa0f3d15eba27495395d8af204e351dd143
Marko <marko.milosevic.offic@gmail.com>
2023-11-10 00:48:55 +0100

    Common1-main_deleted

--- src/common/Common.java
+++ src/common/Common.java
@@ -11,11 +11,6 @@ package common;
  */
 public class Common {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+
     
 }
